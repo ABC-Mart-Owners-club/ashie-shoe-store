5b673b278838c5835551658fd41b75a1d2834734
ashlovesliitea <skyrider1811@gmail.com>
2025-04-13 11:12:53 +0900

    :tada: init project

---
+++ src/main/java/com/abc/mart/AbcMartApplication.java
@@ -0,0 +1,13 @@
+package com.abc.mart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcMartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcMartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/abc/mart/AbcMartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abc.mart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcMartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
