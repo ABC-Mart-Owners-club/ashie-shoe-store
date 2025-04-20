f9d9765bd87592b39c6a76535d3164d40955d68c
Jiyul Lee <skyrider1811@gmail.com>
2025-04-20 22:26:08 +0900

    :sparkles: add readOnly

--- src/main/java/com/abc/mart/order/usecase/CalculateSalesAmountUsecase.java
+++ src/main/java/com/abc/mart/order/usecase/CalculateSalesAmountUsecase.java
@@ -3,15 +3,15 @@ package com.abc.mart.order.usecase;
 import com.abc.mart.order.domain.repository.OrderRepository;
 import com.abc.mart.order.domain.repository.OrderSpecification;
 import com.abc.mart.order.usecase.dto.CalculateSalesRequest;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class CalculateSalesAmountUsecase {
 
     private final OrderRepository orderRepository;
 
-    @Transactional
+    @Transactional(readOnly = true)
     public long calculateSalesAmount(CalculateSalesRequest request) {
         var orders = orderRepository.findByTerm(OrderSpecification.between(request.from(), request.to()));
 
