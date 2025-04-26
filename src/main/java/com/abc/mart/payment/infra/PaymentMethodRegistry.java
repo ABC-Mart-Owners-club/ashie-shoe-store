00980f53e715713ac3a77182fd2c8c9882493c90
Jiyul Lee <65891711+ashlovesliitea@users.noreply.github.com>
2025-04-27 02:06:15 +0900

    Update comment on PaymentMethodRegistry.java

--- src/main/java/com/abc/mart/payment/infra/PaymentMethodRegistry.java
+++ src/main/java/com/abc/mart/payment/infra/PaymentMethodRegistry.java
@@ -7,7 +7,7 @@ import java.util.EnumMap;
 import java.util.List;
 import java.util.Map;
 
-//전략 패턴 활용하여 PaymentMethod들의 구현체를 동적으로 구현하고, 필요한 구현체를 꺼내 쓸 수 있도록 함
+//전략 패턴 활용하여 PaymentMethod들의 구현체를 동적으로 등록하고, 필요한 구현체를 꺼내 쓸 수 있도록 함
 @Component
 public class PaymentMethodRegistry {
     private final Map<PaymentMethodType, PaymentMethod> registry = new EnumMap<>(PaymentMethodType.class);
